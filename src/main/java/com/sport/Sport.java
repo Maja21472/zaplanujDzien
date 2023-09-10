b26c21eedbb3b3700553dcc3212c6f9b38dd2da7
Maja21472 <wwx15367@student.warszawa.wsb.pl>
2023-09-10 18:10:57 +0200

    rospoczynamy prace nad planem aktywności

---
+++ src/main/java/com/sport/Sport.java
@@ -0,0 +1,14 @@
+package com.sport;
+
+public class Sport {
+    private final String[] aktywnosci = {"bieganie", "skakanka", "siłownia", "basen", "rolki", "wrotki", "spacer"};
+
+    /**
+     * Metoda generująca aktywność sportową w zależności od rzeczywistego dnia tygodnia.
+     *
+     * @return Wygenerowana aktywność sportowa.
+     */
+    public String generujAktywnosc() {
+
+    }
+}
